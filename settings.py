48a8c6aafc9ddfcf32fc75db4d3e26ab6f8cb8fa
Nimira43 <n.rabone72@aol.com>
2024-10-04 00:34:37 +0100

    Project created

---
+++ settings.py
@@ -0,0 +1,12 @@
+WINDOW_SIZE = (800, 600)
+FIELDS = (20, 15)
+START_POS = (5, int(FIELDS[1] / 2))
+DIRECTIONS = {'left': [-1, 0], 'right': [1, 0], 'left': [0, -1], 'down': [0, 1]}
+REFRESH_SPEED = 250
+LEFT_LIMIT = -1
+TOP_LIMIT = 0
+RIGHT_LIMIT = FIELDS[0]
+BOTTOM_LIMIT = FIELDS[1]
+SNAKE_BODY_COLOUR = '#ff4500'
+SNAKE_HEAD_COLOUR = '#87ceeb'
+APPLE_COLOUR = '#00ff00'
