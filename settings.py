39d46cd2bd4b1089baa37154cdb698db3f84b79c
Nimira43 <n.rabone72@aol.com>
2024-10-05 20:44:11 +0100

    Updated

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ FIELDS = (20, 15)
 START_POS = (5, int(FIELDS[1] / 2))
 DIRECTIONS = {'left': [-1, 0], 'right': [1, 0], 'left': [0, -1], 'down': [0, 1]}
 REFRESH_SPEED = 250
+
 LEFT_LIMIT = -1
 TOP_LIMIT = 0
 RIGHT_LIMIT = FIELDS[0]
