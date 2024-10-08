43c3f67e0a19121a54853ba485d7c10dd81a6824
Nimira43 <n.rabone72@aol.com>
2024-10-09 00:56:52 +0100

    Set size of game window

--- snake.py
+++ snake.py
@@ -1,8 +1,12 @@
 import customtkinter as ctk
+from settings import *
+
 
 class Game(ctk.CTk):
   def __init__(self):
     super().__init__()
+    self.title('Python vs Snake')
+    self.geometry(f'{WINDOW_SIZE[0]}x{WINDOW_SIZE[1]}')
     self.mainloop()
 
 Game()
