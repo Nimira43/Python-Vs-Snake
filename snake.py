2df74afeb7f1957e8c47477e4260587fe1d0d4ff
Nimira43 <n.rabone72@aol.com>
2024-10-09 01:18:03 +0100

    Setting up grid and snake

--- snake.py
+++ snake.py
@@ -1,12 +1,18 @@
 import customtkinter as ctk
 from settings import *
 
-
 class Game(ctk.CTk):
   def __init__(self):
     super().__init__()
     self.title('Python vs Snake')
     self.geometry(f'{WINDOW_SIZE[0]}x{WINDOW_SIZE[1]}')
+    
+    self.columnconfigure(list(range(FIELDS[0])), weight = 1, uniform = 'a')
+    self.rowconfigure(list(range(FIELDS[1])), weight = 1, uniform = 'a')
+    
+    self.snake = [START_POS, (START_POS[0] - 1, START_POS[1]), (START_POS[0] - 2, START_POS[1])]
+
+
     self.mainloop()
 
 Game()
