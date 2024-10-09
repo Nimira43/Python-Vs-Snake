b6c1c93cfa6428c8ae8f6ff33f11988e19c05963
Nimira43 <n.rabone72@aol.com>
2024-10-09 02:28:32 +0100

    created function for placing apples at random places on the screen

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from settings import *
+from random import randint
 
 class Game(ctk.CTk):
   def __init__(self):
@@ -11,15 +12,21 @@ class Game(ctk.CTk):
     self.rowconfigure(list(range(FIELDS[1])), weight = 1, uniform = 'a')
     
     self.snake = [START_POS, (START_POS[0] - 1, START_POS[1]), (START_POS[0] - 2, START_POS[1])]
+    
+    self.place_apple()    
+    self.draw_frames = []
     self.draw()
-
     self.mainloop()
-  
+
+  def place_apple(self):
+    self.apple_pos = {randint(0, FIELDS[0] - 1), randint(0, FIELDS[1] - 1) }
+
   def draw(self):
     for index, pos in enumerate(self.snake):
       colour = SNAKE_BODY_COLOUR if index != 0 else SNAKE_HEAD_COLOUR
       snake_frame = ctk.CTkFrame(self, fg_color= colour, corner_radius = 0)
-      snake_frame.grid(column = pos[0], row = pos[1])
-
+      # snake_frame.grid(column = pos[0], row = pos[1])
+      self.draw_frames.append((snake_frame, pos))
+    for frame, pos in self.draw_frames
 
 Game()
